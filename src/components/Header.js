1a72b08ec836a4ddcd59700f9c98d940fbc6388a
minhajul-karim <minhajul.kaarim@gmail.com>
2021-01-27 12:07:50 +0600

    Remove test paragraph from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -63,17 +63,6 @@ export default function Header() {
           </li>
         </ul>
       </nav>
-      <p style={{ backgroundColor: 'yellow' }}>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio, earum? Doloribus
-        consequuntur eaque earum cum expedita numquam laborum sint. Omnis porro incidunt dolorum
-        nostrum doloremque iure ullam quae a soluta, accusamus debitis quod mollitia corporis!
-        Aperiam repudiandae quasi modi quisquam assumenda, sed hic minima nisi. Magnam vero
-        doloribus eos quasi illo placeat ut error, recusandae aperiam adipisci quibusdam a
-        voluptatem magni est blanditiis autem delectus quo ratione optio dolorum. Id, dolor a ad
-        dignissimos autem ipsam officiis sed inventore, odit doloribus officia labore consequatur
-        veritatis deserunt placeat accusantium. Cupiditate vitae quia, consequatur tenetur quisquam
-        natus alias quos voluptatibus quidem nesciunt.
-      </p>
     </>
   )
 }
