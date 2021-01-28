689e2cb2dd52ffa389f9c4a412bf6619d1e5abaf
minhajul-karim <minhajul.kaarim@gmail.com>
2021-01-28 16:13:37 +0600

    Add copy button to grids

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,7 +1,9 @@
 export default function Grid({ color }) {
   const { name, hex, hexClean, rgb, rgba } = color
+  console.log(rgba)
   return (
     <div className="single-grid-cell" style={{ backgroundColor: hex }}>
+      <div className="copy-btn" />
       <p className="color-name">{name}</p>
     </div>
   )
