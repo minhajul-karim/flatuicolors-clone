c31935e4785aff1bfc722ef15e5da92340e8c66f
minhajul-karim <minhajul.kaarim@gmail.com>
2021-02-02 17:30:12 +0600

    Success message spreads full screen

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,20 +1,24 @@
+/* eslint-disable jsx-a11y/media-has-caption */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* Grid cells are not tabbable */
 
+import React from 'react'
 import { useGlobalContext } from '../context'
 import copySound from '../assets/sounds/copy_sound.m4a'
 
 export default function Grid({ color }) {
   const { name, hex } = color
-  const { selectedColorFormat, setColor, setShowSuccessMsg, isSoundOn } = useGlobalContext()
+  const { selectedColorFormat, isSoundOn, setColor, setShowSuccessMsg } = useGlobalContext()
 
-  // Copy code to clipboard
+  // Copy code to clipboard and play sound
   const copyHandler = () => {
     navigator.clipboard.writeText(color[selectedColorFormat.name])
     setColor(color)
     setShowSuccessMsg(true)
+    // Scroll to top so that the success msg can spread
+    window.scrollTo(0, 0)
     const audio = new Audio(copySound)
     isSoundOn && audio.play()
   }
